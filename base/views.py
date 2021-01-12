155e7f53691c63cbb42d72b34925ec0180ff635d
muskanvaswan <muskanvaswan@gmail.com>
2021-01-12 23:04:09 +0530

    initialising viws for login and sign_up

--- base/views.py
+++ base/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-   return render(request,'base/index.html')
+    return render(request, 'base/index.html')
+
+
+def sign_in(request):
+    return render(request, 'base/login.html')
+
+
+def sign_up(request):
+    return render(request, 'base/sign_up.html')
