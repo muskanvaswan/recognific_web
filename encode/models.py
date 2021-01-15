4a8ff73d276e84408a1476c3f9804bda490de9e0
muskanvaswan <muskanvaswan@gmail.com>
2021-01-15 15:46:12 +0530

    fixing post_init signal not working

--- encode/models.py
+++ encode/models.py
@@ -1,25 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_init
 from django.dispatch import receiver
 
 import numpy as np
 import pickle
 import json
+import time
 
 from .encodings import encode_image
 
 
 def make_encodings(instance):
     c = instance
+    print(c)
     IMAGES_LIST = [student.encodings['e'] for student in c.students.all()]
     ORDER_LIST = [student.id for student in c.students.all()]
+    # print(ORDER_LIST)    # print(IMAGES_LIST)
 
     encodings = np.array(IMAGES_LIST)
 
     #encodings = np.array(list(map(encode_image, IMAGES_LIST)))
     encodings = {"encodings": encodings, "order": ORDER_LIST}
-
+    # print("here")
     # opening file in write mode (binary)
     file = open(f"encodings/class{c.id}.txt", "wb+")
 
@@ -58,5 +61,17 @@ class ClassSet(models.Model):
 
 @receiver(post_save, sender=ClassSet)
 def my_handler(sender, **kwargs):
-    print(kwargs.get("instance"))
+    #print("on post save")
+    # if kwargs.get('created') == True:
+    # time.sleep(10)
+    # kwargs.get("instance").save()
+
+    print(kwargs.get("created"))
     make_encodings(kwargs.get("instance"))
+
+
+# @receiver(post_init, sender=ClassSet)
+# def my_handler(sender, instance, **kwargs):
+    # if kwargs.get("created"):
+    # print(kwargs.get("instance"))
+    # make_encodings(kwargs.get("instance"))
