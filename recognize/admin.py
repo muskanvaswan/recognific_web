fa91325086ac7738970f011ab11e76f31924bace
muskanvaswan <muskanvaswan@gmail.com>
2021-01-15 12:57:20 +0530

    making time visible in admin attendance

--- recognize/admin.py
+++ recognize/admin.py
@@ -3,4 +3,19 @@ from django.contrib import admin
 from .models import Attendance
 
 # Register your models here.
-admin.site.register(Attendance)
+
+
+class AttendanceAdmin(admin.ModelAdmin):
+    fields = [
+        'classname',
+        'time',
+        'student',
+    ]
+
+    readonly_fields = ['time', ]
+
+    class Meta:
+        model = Attendance
+
+
+admin.site.register(Attendance, AttendanceAdmin)
