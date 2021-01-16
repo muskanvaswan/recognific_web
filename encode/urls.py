1c40f732debfeb7fcafdfcb23c2fe5e06519d580
muskanvaswan <muskanvaswan@gmail.com>
2021-01-16 15:11:03 +0530

    changing url for classset create

--- encode/urls.py
+++ encode/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ClassSetCreateView.as_view(), name="create_classset"),
+    path('classset/create/', views.ClassSetCreateView.as_view(), name="create_classset"),
 ]
