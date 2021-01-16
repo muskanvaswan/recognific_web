f4f356b7acbf3a5784796c265bbf791e4f1cb2c5
arushisingla <arushisingla1911@gmail.com>
2021-01-17 00:10:47 +0530

    Making logout page

--- base/urls.py
+++ base/urls.py
@@ -6,8 +6,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-
-    path('sign_up/', views.sign_up, name="sign_up"),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name="base/accounts/login.html"),name="login"),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name="base/accounts/logout.html"),name="logout"),
+    path('accounts/sign_up/', views.sign_up, name="sign_up"),
     path('about/', views.about, name="about")
 ]
-#path('login/', auth_views.login, name="login"),
--- base/views.py
+++ base/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from django.contrib.auth import login, authenticate
+from base.forms import SignUpForm
 
 def index(request):
+    print(request.user)
     return render(request, 'base/index.html')
 
 
 def sign_in(request):
     return render(request, 'base/login.html')
 
-
-def sign_up(request):
-    return render(request, 'base/sign_up.html')
-
 def about(request):
     return render(request, 'base/about.html')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
