e015307eaba1610926bd3722949188dba2a3f672
muskanvaswan <muskanvaswan@gmail.com>
2021-01-16 21:09:12 +0530

    setting up view for student create view

--- encode/views.py
+++ encode/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.edit import CreateView
-from .models import ClassSet
+from .models import ClassSet, Student
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
@@ -8,4 +8,11 @@ class ClassSetCreateView(LoginRequiredMixin, CreateView):
     model = ClassSet
     success_url = reverse_lazy('index')
     fields = ['name', 'students', 'teacher']
-    template_name = 'encode/make_classset.html'
+    template_name = 'encode/create.html'
+
+
+class StudentCreateView(LoginRequiredMixin, CreateView):
+    model = Student
+    success_url = reverse_lazy('index')
+    fields = ['first_name', 'last_name', 'image']
+    template_name = 'encode/create.html'
