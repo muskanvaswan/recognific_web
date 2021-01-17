61db16eaaeef55e76cfd3c37f441e17a0f693e42
muskanvaswan <muskanvaswan@gmail.com>
2021-01-17 18:23:53 +0530

    fixing import for class based views

--- encode/urls.py
+++ encode/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name="dashboard"),
+    path('', views.DashboardView.as_view(), name="dashboard"),
     path('classset/create/', views.ClassSetCreateView.as_view(), name="create_classset"),
     path('student/create/', views.StudentCreateView.as_view(), name="create_student"),
 
--- encode/views.py
+++ encode/views.py
@@ -1,4 +1,5 @@
-from django.views.generic.edit import CreateView, TemplateView
+from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
 from .models import ClassSet, Student
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
