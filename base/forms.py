295cbb4d2ea76fdaeb60df6dd16d7b695a0b6623
muskanvaswan <muskanvaswan@gmail.com>
2021-01-19 10:17:04 +0530

    fixing sign up form

--- base/forms.py
+++ base/forms.py
@@ -1,13 +1,10 @@
 from django import forms
 #from django.contrib.auth.forms import ModelForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
-class SignUpForm(forms.ModelForm):
-    #first_name = forms.CharField(max_length=30, required=True)
-    #last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    #email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-
+class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password', )
+        fields = ('email', 'username', 'password1', 'password2', 'first_name', 'last_name')
--- base/views.py
+++ base/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import login, authenticate,logout
+from django.contrib.auth import login, authenticate, logout
 from base.forms import SignUpForm
 from django.urls import reverse_lazy
-
 
 
 def index(request):
@@ -12,31 +11,34 @@ def index(request):
 
 
 def sign_in(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             # Redirect to a success page.
-            #return render(request,'encode/dashboard.html')
+            # return render(request,'encode/dashboard.html')
             return redirect('/dashboard/')
         else:
             # Return an 'invalid login' error message.
-            context={"message": "Invalid Credentials"}
-            return render(request,'base/accounts/login.html', context)
-    else :
+            context = {"message": "Invalid Credentials"}
+            return render(request, 'base/accounts/login.html', context)
+    else:
         if request.user.is_authenticated:
             return reverse_lazy('index')
         else:
             return render(request, 'base/accounts/login.html')
 
+
 def log_out_view(request):
     logout(request)
     return redirect('/accounts/login/')
 
+
 def about(request):
     return render(request, 'base/about.html')
+
 
 def sign_up(request):
     if request.method == 'POST':
@@ -44,10 +46,12 @@ def sign_up(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
+            raw_password = form.cleaned_data.get('password2')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect("/")
+        else:
+            return redirect("/accounts/sign_up/")
     else:
         form = SignUpForm()
         return render(request, 'base/accounts/sign_up.html', {'form': form})
