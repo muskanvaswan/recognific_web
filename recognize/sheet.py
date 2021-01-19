1a2bf119b790dc542b669e5139acc41558b6551e
muskanvaswan <muskanvaswan@gmail.com>
2021-01-19 15:57:09 +0530

    added funtionality for month attendance sheet

--- recognize/sheet.py
+++ recognize/sheet.py
@@ -1,15 +1,26 @@
 import datetime
 
-from .query import attendance_day
+from .query import attendance_day, attendance_month
 from .reader import csv_writter, excel_writer
 
 
 def attendance_sheet_day(date, classname):
     date = datetime.datetime(2021, 1, 15).timetuple()
     qs = attendance_day(date, classname)
-    print("qs", qs)
     file = open(f"Attendance/sheet_{classname}.csv", 'w+')
     file.write(f'{date[0]}/{date[1]}/{date[2]}, {classname}\n')
+    file.write('Name, Date and Time \n')
+    file.close()
+    for q in qs:
+        ls = [q.student.get_full_name(), q.time]
+        csv_writter(ls, classname)
+    excel_writer(classname)
+
+
+def attendance_sheet_month(month, classname):
+    qs = attendance_month(month, classname)
+    file = open(f"Attendance/sheet_{classname}.csv", 'w+')
+    file.write(f'for month {month}, {classname}\n')
     file.write('Name, Date and Time \n')
     file.close()
     for q in qs:
