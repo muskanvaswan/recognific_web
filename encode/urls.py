87cbe13314aa8d37d5024fba46bc74f8496c05ad
muskanvaswan <muskanvaswan@gmail.com>
2021-01-20 21:08:00 +0530

    add class detail url

--- encode/urls.py
+++ encode/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.DashboardView.as_view(), name="dashboard"),
     path('classset/create/', views.ClassSetCreateView.as_view(), name="create_classset"),
     path('student/create/', views.StudentCreateView.as_view(), name="create_student"),
+    path('class/<int:classset_id>/', views.ClassSetDetailView.as_view(), name="class_details"),
 
 ]
