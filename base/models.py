727dbf3818744d54a16bdc500d53a1b1e0d1407a
muskanvaswan <muskanvaswan@gmail.com>
2021-01-22 20:50:18 +0530

    making a teacher model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+class Teacher(models.Model):
+    user = models.ForeignKey(User, related_name='user', unique=True)
+    role = models.CharField(blank=True)
