cce3ea30cc7c728d41efc4d3524fa3567cf15f02
muskanvaswan <muskanvaswan@gmail.com>
2021-01-22 20:51:25 +0530

    fixing syntax error

--- base/models.py
+++ base/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 
 
 class Teacher(models.Model):
-    user = models.ForeignKey(User, related_name='user', unique=True on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user', unique=True, on_delete=models.CASCADE)
     role = models.CharField(blank=True)
