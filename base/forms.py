144a67056c3148c7f6b77379f389443a5c300db4
muskanvaswan <muskanvaswan@gmail.com>
2021-02-02 12:35:08 +0530

    commenting uneccasary form out

--- base/forms.py
+++ base/forms.py
@@ -1,11 +1,10 @@
 from django import forms
 #from django.contrib.auth.forms import ModelForm
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from encode.models import Student
-
-
-class StudentCreateForm(forms.ModelForm):
-    class Meta:
-        model = Student
-        fields = ['image']
+# from django.contrib.auth.models import User
+# from django.contrib.auth.forms import UserCreationForm
+# from encode.models import Student
+#
+#
+# class StudentCreateForm(forms.ModelForm):
+# class Meta:
+# model = Student
