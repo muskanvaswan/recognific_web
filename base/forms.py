9fc70245eec7e51c1e9442b7bc35afe531434b95
muskanvaswan <muskanvaswan@gmail.com>
2021-02-02 12:25:20 +0530

    testing form usage

--- base/forms.py
+++ base/forms.py
@@ -2,9 +2,10 @@ from django import forms
 #from django.contrib.auth.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from encode.models import Student
 
 
-class SignUpForm(UserCreationForm):
+class StudentCreateForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ('email', 'username', 'password1', 'password2', 'first_name', 'last_name')
+        model = Student
+        fields = ['image']
