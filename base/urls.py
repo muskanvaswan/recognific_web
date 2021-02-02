5737c6079e98bd26e703030fa4885d30d0bc7047
muskanvaswan <muskanvaswan@gmail.com>
2021-02-02 12:24:04 +0530

    urls for student/teacher sign in

--- base/urls.py
+++ base/urls.py
@@ -6,9 +6,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('accounts/login/',views.sign_in,name="login"),
-    path('accounts/logout/', views.log_out_view,name="logout"),
+    path('accounts/login/', views.sign_in, name="login"),
+    path('accounts/logout/', views.log_out_view, name="logout"),
     path('accounts/sign_up/', views.sign_up, name="sign_up"),
+    path('accounts/sign_up_as/', views.sign_up_as, name="sign_up_as"),
+
     path('about/', views.about, name="about"),
     path('contact/', views.contact, name="contact")
 
