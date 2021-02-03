735e527223ba21d5bb32e239828df27adbdf9af5
muskanvaswan <muskanvaswan@gmail.com>
2021-02-03 12:51:17 +0530

    making form with user creation and first name, last name fields

--- base/forms.py
+++ base/forms.py
@@ -1,10 +1,18 @@
 from django import forms
+from django.forms import ModelForm
 #from django.contrib.auth.forms import ModelForm
-# from django.contrib.auth.models import User
-# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 # from encode.models import Student
 #
 #
 # class StudentCreateForm(forms.ModelForm):
 # class Meta:
 # model = Student
+
+
+class UserCreateForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email',)
