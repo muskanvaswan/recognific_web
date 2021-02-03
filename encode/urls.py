da40afabc0e1444e93b5197c41cb8a1d854b7996
muskanvaswan <muskanvaswan@gmail.com>
2021-02-03 12:33:41 +0530

    update url for classset

--- encode/urls.py
+++ encode/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.DashboardView.as_view(), name="dashboard"),
     path('classset/create/', views.ClassSetCreateView.as_view(), name="create_classset"),
+    path('classset/update/<pk>/', views.ClassSetUpdateView.as_view(), name="update_classset"),
+
     path('student/create/', views.StudentCreateView.as_view(), name="create_student"),
     path('class/<int:classset_id>/', views.ClassSetDetailView.as_view(), name="class_details"),
     path('class/activate/<int:classset_id>/', views.activate_class_set, name="activate"),
