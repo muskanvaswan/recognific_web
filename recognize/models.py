f4c509b2ade246bed32b2dff2a05ac7cf45d4ba1
Muskan Vaswan <muskanvaswan@gmail.com>
2021-02-09 11:30:33 +0530

    fixing timezone

--- recognize/models.py
+++ recognize/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from django.utils import timezone
 from encode.models import Student, ClassSet
 
 
 class Attendance(models.Model):
     classname = models.ForeignKey(ClassSet, on_delete=models.CASCADE, related_name="attendees")
     student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="attendance")
-    time = models.DateTimeField(auto_now_add=True)
+    time = models.DateTimeField(default=timezone.now)
 
 
 class Marked(models.Model):
